bfa1a200e1a7a19ac62def41c6ec1167ddc45001
LindaPostoli <linda.postoli@gmail.com>
2016-01-11 15:33:19 +0100

    Added some attributes and the method toCSV()

--- src/main/java/it/synclab/hrpm/model/Candidate.java
+++ src/main/java/it/synclab/hrpm/model/Candidate.java
@@ -5,29 +5,35 @@ import java.util.Calendar;
 public class Candidate {
 
 	private String taxCode;
-	private String surname;
-	private String name;
+	private String name, surname;
 	private Calendar birthDate;
 	private String birthPlace;
-	private String address;
-	private String phoneNumber; // TO DO: control phoneNumber.length()=10
-	private String eMail;
+	private String address, zipCode, city, country;
+	public static final String HEADER = "NAME; SURNAME; TAX_CODE; BIRTH_DATE; BIRTH_PLACE; ADDRESS; ZIP_CODE; CITY; COUNTRY";
+
+	private String phoneNumber, eMail; // TODO: control phoneNumber.length()=10
 
 	private Rating rating;
-	// TO DO: private enum channel
+	// TODO: private enum channel
 
-	public Candidate(String taxCode, String surname, String name, Calendar birthDate, String birthPlace, String address,
-			String phoneNumber, String eMail, Rating rating) {
-		super();
+	public Candidate(String taxCode, String name, String surname, Calendar birthDate, String birthPlace, String address,
+			String zipCode, String city, String country, String phoneNumber, String eMail, Rating rating) {
 		this.taxCode = taxCode;
-		this.surname = surname;
 		this.name = name;
+		this.surname = surname;
 		this.birthDate = birthDate;
 		this.birthPlace = birthPlace;
 		this.address = address;
+		this.zipCode = zipCode;
+		this.city = city;
+		this.country = country;
 		this.phoneNumber = phoneNumber;
 		this.eMail = eMail;
 		this.rating = rating;
+	}
+
+	public Candidate(String taxCode) {
+		this.taxCode = taxCode;
 	}
 
 	public String getTaxCode() {
@@ -38,20 +44,20 @@ public class Candidate {
 		this.taxCode = taxCode;
 	}
 
-	public String getSurname() {
-		return surname;
-	}
-
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-
 	public String getName() {
 		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
 	}
 
 	public Calendar getBirthDate() {
@@ -76,6 +82,30 @@ public class Candidate {
 
 	public void setAddress(String address) {
 		this.address = address;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
 	}
 
 	public String getPhoneNumber() {
@@ -106,9 +136,18 @@ public class Candidate {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((birthDate == null) ? 0 : birthDate.hashCode());
+		result = prime * result + ((birthPlace == null) ? 0 : birthPlace.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + ((eMail == null) ? 0 : eMail.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((phoneNumber == null) ? 0 : phoneNumber.hashCode());
+		result = prime * result + ((rating == null) ? 0 : rating.hashCode());
 		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
 		result = prime * result + ((taxCode == null) ? 0 : taxCode.hashCode());
+		result = prime * result + ((zipCode == null) ? 0 : zipCode.hashCode());
 		return result;
 	}
 
@@ -121,10 +160,50 @@ public class Candidate {
 		if (getClass() != obj.getClass())
 			return false;
 		Candidate other = (Candidate) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (birthDate == null) {
+			if (other.birthDate != null)
+				return false;
+		} else if (!birthDate.equals(other.birthDate))
+			return false;
+		if (birthPlace == null) {
+			if (other.birthPlace != null)
+				return false;
+		} else if (!birthPlace.equals(other.birthPlace))
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (eMail == null) {
+			if (other.eMail != null)
+				return false;
+		} else if (!eMail.equals(other.eMail))
+			return false;
 		if (name == null) {
 			if (other.name != null)
 				return false;
 		} else if (!name.equals(other.name))
+			return false;
+		if (phoneNumber == null) {
+			if (other.phoneNumber != null)
+				return false;
+		} else if (!phoneNumber.equals(other.phoneNumber))
+			return false;
+		if (rating == null) {
+			if (other.rating != null)
+				return false;
+		} else if (!rating.equals(other.rating))
 			return false;
 		if (surname == null) {
 			if (other.surname != null)
@@ -136,14 +215,25 @@ public class Candidate {
 				return false;
 		} else if (!taxCode.equals(other.taxCode))
 			return false;
+		if (zipCode == null) {
+			if (other.zipCode != null)
+				return false;
+		} else if (!zipCode.equals(other.zipCode))
+			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Candidate [taxCode=" + taxCode + ", surname=" + surname + ", name=" + name + ", birthDate=" + birthDate
-				+ ", birthPlace=" + birthPlace + ", address=" + address + ", phoneNumber=" + phoneNumber + ", eMail="
-				+ eMail + ", rating=" + rating + "]";
+		return "Candidate [taxCode=" + taxCode + ", name=" + name + ", surname=" + surname + ", birthDate=" + birthDate
+				+ ", birthPlace=" + birthPlace + ", address=" + address + ", zipCode=" + zipCode + ", city=" + city
+				+ ", country=" + country + ", phoneNumber=" + phoneNumber + ", eMail=" + eMail + ", rating=" + rating
+				+ "]";
+	}
+
+	public String toCSV() {
+		return name + ";" + surname + ";" + taxCode + ";" + birthDate + ";" + birthPlace + ";" + address + ";" + zipCode
+				+ city + ";" + country + ";";
 	}
 
 }
