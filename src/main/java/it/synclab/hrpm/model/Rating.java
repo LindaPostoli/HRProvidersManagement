0778c7cd2b25df507fe2154180bd9556a2fe78c5
LindaPostoli <linda.postoli@gmail.com>
2016-01-11 18:12:53 +0100

    added header, toCSV()

--- src/main/java/it/synclab/hrpm/model/Rating.java
+++ src/main/java/it/synclab/hrpm/model/Rating.java
@@ -1,30 +1,41 @@
 package it.synclab.hrpm.model;
 
 public class Rating {
-	
+
 	private String professionalImpressions;
 	private String personalityImpressions;
 	private int mark;
-	//TO DO: insert attributo per il numero di colloqui (se più di uno)
-	
+	private static final String HEADER = "PROFESSIONAL_IMPRESSIONS; PERSONALITY_IMPRESSIONS; MARK;";
+	// TODO: insert attributo per il numero di colloqui (se più di uno)
+
 	public String getProfessionalImpressions() {
 		return professionalImpressions;
 	}
+
 	public void setProfessionalImpressions(String professionalImpressions) {
 		this.professionalImpressions = professionalImpressions;
 	}
+
 	public String getPersonalityImpressions() {
 		return personalityImpressions;
 	}
+
 	public void setPersonalityImpressions(String personalityImpressions) {
 		this.personalityImpressions = personalityImpressions;
 	}
+
 	public int getMark() {
 		return mark;
 	}
+
 	public void setMark(int mark) {
 		this.mark = mark;
 	}
+
+	public static String getHeader() {
+		return HEADER;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -34,6 +45,7 @@ public class Rating {
 		result = prime * result + ((professionalImpressions == null) ? 0 : professionalImpressions.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -57,13 +69,11 @@ public class Rating {
 			return false;
 		return true;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Rating [professionalImpressions=" + professionalImpressions + ", personalityImpressions="
 				+ personalityImpressions + ", mark=" + mark + "]";
 	}
-	
-	
 
 }
