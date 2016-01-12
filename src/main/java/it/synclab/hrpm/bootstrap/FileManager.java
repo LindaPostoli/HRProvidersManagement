fa4aa1a066ea880865e2b0acdbda782a0309ed1d
LindaPostoli <linda.postoli@gmail.com>
2016-01-12 14:52:21 +0100

    changed channelFileName from static to dinamic

--- src/main/java/it/synclab/hrpm/bootstrap/FileManager.java
+++ src/main/java/it/synclab/hrpm/bootstrap/FileManager.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import it.synclab.hrpm.enumeration.ChannelType;
+import it.synclab.hrpm.factory.ChannelFactory;
 import it.synclab.hrpm.model.Candidate;
 import it.synclab.hrpm.model.Channel;
 import it.synclab.hrpm.model.Entity;
@@ -12,21 +14,18 @@ import it.synclab.hrpm.model.Rating;
 public class FileManager {
 
 	private static String candidateFileName = Candidate.class.getSimpleName() + ".csv";
-	private String channelFileName = Channel.class.getSimpleName() + ".csv";
 	private static String ratingFileName = Rating.class.getSimpleName() + ".csv";
 	public static final String BASEPATH = "hr" + File.separator;
 
 	private static void newFile(String fileName, String header) {
 		File file = new File(BASEPATH + fileName);
-		if (file.exists())
+		if (file.exists() || header == null)
 			return;
 
 		FileWriter fw = null;
 		try {
-			boolean mkdirs = false;
-			if (!file.getParentFile().exists()) {
-				mkdirs = file.getParentFile().mkdirs();
-			}
+			if (!file.getParentFile().exists())
+				file.getParentFile().mkdirs();
 			file.createNewFile();
 			fw = new FileWriter(file);
 			fw.write(header + "\n");
@@ -34,15 +33,19 @@ public class FileManager {
 			fw.close();
 
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
 	public static final void init() {
 		newFile(candidateFileName, Candidate.HEADER);
-		// newFile(channelFileName, Channel.HEADER);
-		// newFile(ratingFileName, Rating.HEADER);
+
+		for (ChannelType channelType : ChannelType.values()) {
+			Channel channel = ChannelFactory.getInstance(channelType);
+			newFile(channel.getClass().getSimpleName() + ".csv", channel.getHeader());
+		}
+
+		newFile(ratingFileName, Rating.HEADER);
 	}
 
 	public void insert(Entity obj) {
