8d3d8ad35f947f72eeda38260276c5a089508e66
LindaPostoli <linda.postoli@gmail.com>
2016-01-12 11:50:06 +0100

    fixed main

--- src/main/java/it/synclab/hrpm/main/HRManagement.java
+++ src/main/java/it/synclab/hrpm/main/HRManagement.java
@@ -1,6 +1,9 @@
 package it.synclab.hrpm.main;
 
 
+import java.text.ParseException;
+
+import it.synclab.hrpm.bootstrap.FileManager;
 import it.synclab.hrpm.enumeration.ChannelType;
 import it.synclab.hrpm.enumeration.ConnectionCriteria;
 import it.synclab.hrpm.factory.ChannelFactory;
@@ -15,23 +18,26 @@ public class HRManagement {
 		bootstrap();
 	}
 
-	private static void bootstrap() {
+	private static void bootstrap() throws ParseException {
 		bootstrap(ConnectionCriteria.FILE);
 	}
 
-	private static void bootstrap(ConnectionCriteria criteria) {
+	private static void bootstrap(ConnectionCriteria criteria) throws ParseException {
 		if (criteria == ConnectionCriteria.FILE)
 			FileManager.init();
 
 		CandidateService cs = new CandidateService(criteria);
-		Candidate candidate = new Candidate("Cxxsswwww");
+		ChannelFactory cf= new ChannelFactory();
+		Channel synclab= cf.getInstance(ChannelType.COMPANY);
+		synclab.setName("SyncLab");
+		Candidate candidate= new Candidate("pstlnd87z100l", synclab);
+		candidate.setBirthDateString("03/05/1987");
 		cs.insert(candidate);
+		
+		System.out.println(candidate.getChannel().toCSV());
 	}
 
 	
-	ChannelFactory cf= new ChannelFactory();
-	Channel synclab= cf.getInstance(ChannelType.COMPANY);
-	Candidate linda= new Candidate("pstlnd87z100l", synclab);
 	
 	
 	
