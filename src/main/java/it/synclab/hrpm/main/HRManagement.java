4bcbe4ccd935faa839a9766a74555e653c806530
LindaPostoli <linda.postoli@gmail.com>
2016-01-12 10:08:44 +0100

    tested FactoryChannel

--- src/main/java/it/synclab/hrpm/main/HRManagement.java
+++ src/main/java/it/synclab/hrpm/main/HRManagement.java
@@ -1,6 +1,11 @@
 package it.synclab.hrpm.main;
 
+import it.synclab.hrpm.enumeration.ChannelType;
 import it.synclab.hrpm.enumeration.ConnectionCriteria;
+import it.synclab.hrpm.factory.ChannelFactory;
+import it.synclab.hrpm.model.Candidate;
+import it.synclab.hrpm.model.Channel;
+import it.synclab.hrpm.model.Company;
 import it.synclab.hrpm.services.CandidateService;
 
 public class HRManagement {
@@ -13,5 +18,12 @@ public class HRManagement {
 		CandidateService cs = new CandidateService(criteria);
 		// cs.insert(object);
 	}
+	
+	ChannelFactory cf= new ChannelFactory();
+	Channel synclab= cf.getInstance(ChannelType.COMPANY);
+	Candidate linda= new Candidate("pstlnd87z100l", synclab);
+	
+	
+	
 	
 }
