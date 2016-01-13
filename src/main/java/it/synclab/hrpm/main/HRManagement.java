bbc702503bc3a04c9604b6b255d220db7c04b9c0
LindaPostoli <linda.postoli@gmail.com>
2016-01-13 10:41:52 +0100

    adjusted main

--- src/main/java/it/synclab/hrpm/main/HRManagement.java
+++ src/main/java/it/synclab/hrpm/main/HRManagement.java
@@ -8,6 +8,7 @@ import it.synclab.hrpm.enumeration.ConnectionCriteria;
 import it.synclab.hrpm.factory.ChannelFactory;
 import it.synclab.hrpm.model.Candidate;
 import it.synclab.hrpm.model.Company;
+import it.synclab.hrpm.model.UnsolicitedApplication;
 import it.synclab.hrpm.services.CandidateService;
 import it.synclab.hrpm.services.ChannelService;
 
@@ -27,13 +28,19 @@ public class HRManagement {
 		CandidateService cs = new CandidateService(criteria);
 
 		Company synclab = (Company) ChannelFactory.getInstance(ChannelType.COMPANY);
+		
+		
 		synclab.setName("SyncLab");
 		Candidate candidate = new Candidate("hgjfaljv", ChannelType.COMPANY);
+		
+		
 		candidate.setBirthDateString("03/05/1987");
 		cs.insert(candidate);
 		ChannelService chs = new ChannelService(criteria);
 		chs.insert(synclab);
-			
+		
+		Candidate candidate2 = new Candidate("hgjfsssjv", ChannelType.UNSOLICITED_APPLICATION);
+		
 	}
 
 }
