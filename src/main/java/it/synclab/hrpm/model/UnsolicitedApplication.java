b9e1bf1d51cd7833caf0f36adc7f0c410ed875ea
LindaPostoli <linda.postoli@gmail.com>
2016-01-14 12:55:45 +0100

    added id

--- src/main/java/it/synclab/hrpm/model/UnsolicitedApplication.java
+++ src/main/java/it/synclab/hrpm/model/UnsolicitedApplication.java
@@ -2,16 +2,22 @@ package it.synclab.hrpm.model;
 
 public class UnsolicitedApplication implements Channel {
 
+	private static int id=0;
 	public String toCSV() {
 		return "Unsolicited Application";
 	}
 
+	public UnsolicitedApplication(){
+		super();
+		id++;
+	}
 	public String getHeader() {
 		return null;
 	}
-
+	
 	public String getKey() {
-		return null;
+		return String.valueOf(id);
 	}
-
+	
+	
 }
