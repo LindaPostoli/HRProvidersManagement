130d8ea3ef202c1b7ea95e07b438023c22ebbd04
danymar91 <marancinadg@hotmail.it>
2016-01-20 14:48:55 +0100

    added attribute note, changed mark to general

--- src/main/java/it/synclab/hrpm/model/Rating.java
+++ src/main/java/it/synclab/hrpm/model/Rating.java
@@ -5,18 +5,19 @@ public class Rating implements Entity {
 	private int id;
 	private int professionality;
 	private int personality;
-	private int mark;
+	private int general;
+	private String note;
 
-	public static final String HEADER = "ID;PROFESSIONAL_IMPRESSIONS;PERSONALITY_IMPRESSIONS;MARK";
+	public static final String HEADER = "ID;PROFESSIONAL_IMPRESSIONS;PERSONALITY_IMPRESSIONS;GENERAL;NOTE";
 	// TODO: insert attributo per il numero di colloqui (se più di uno)
 
 	public Rating(int id, int professionality, int personality, int mark) {
 		this.id = id;
 		this.professionality = professionality;
 		this.personality = personality;
-		this.mark = mark;
+		this.general = mark;
 	}
-	
+
 	public Rating(String id) {
 		this.id = Integer.parseInt(id);
 	}
@@ -24,12 +25,12 @@ public class Rating implements Entity {
 	public int getId() {
 		return id;
 	}
-	
+
 	/**
 	 * 
 	 * @param id
 	 *            as Integer format
-	 *            
+	 * 
 	 */
 	public void setId(String id) {
 		this.id = Integer.parseInt(id);
@@ -42,12 +43,12 @@ public class Rating implements Entity {
 	public int getProfessionality() {
 		return professionality;
 	}
-	
+
 	/**
 	 * 
 	 * @param Professional
 	 *            as Integer format
-	 *            
+	 * 
 	 */
 	public void setProfessionality(String professionality) {
 		this.professionality = Integer.parseInt(professionality);
@@ -60,12 +61,12 @@ public class Rating implements Entity {
 	public int getPersonality() {
 		return personality;
 	}
-	
+
 	/**
 	 * 
 	 * @param personality
 	 *            as Integer format
-	 *            
+	 * 
 	 */
 	public void setPersonality(String personality) {
 		this.personality = Integer.parseInt(personality);
@@ -75,22 +76,30 @@ public class Rating implements Entity {
 		this.personality = personality;
 	}
 
-	public int getMark() {
-		return mark;
+	public int getGeneral() {
+		return general;
 	}
-	
+
 	/**
 	 * 
 	 * @param mark
 	 *            as Integer format
-	 *            
+	 * 
 	 */
-	public void setMark(String mark) {
-		this.mark = Integer.parseInt(mark);
+	public void setGeneral(String general) {
+		this.general = Integer.parseInt(general);
 	}
 
-	public void setMark(int mark) {
-		this.mark = mark;
+	public void setGeneral(int general) {
+		this.general = general;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
 	}
 
 	public static String getHeader() {
@@ -99,16 +108,17 @@ public class Rating implements Entity {
 
 	@Override
 	public String toString() {
-		return "Rating [id=" + id + ", professionality=" + professionality + ", personality=" + personality + ", mark=" + mark
-				+ "]";
+		return "Rating [id=" + id + ", professionality=" + professionality + ", personality=" + personality
+				+ ", general=" + general + ", note=" + note + "]";
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + general;
 		result = prime * result + id;
-		result = prime * result + mark;
+		result = prime * result + ((note == null) ? 0 : note.hashCode());
 		result = prime * result + personality;
 		result = prime * result + professionality;
 		return result;
@@ -121,11 +131,16 @@ public class Rating implements Entity {
 		if (obj == null)
 			return false;
 		if (getClass() != obj.getClass())
-			return false; 
+			return false;
 		Rating other = (Rating) obj;
+		if (general != other.general)
+			return false;
 		if (id != other.id)
 			return false;
-		if (mark != other.mark)
+		if (note == null) {
+			if (other.note != null)
+				return false;
+		} else if (!note.equals(other.note))
 			return false;
 		if (personality != other.personality)
 			return false;
@@ -135,7 +150,7 @@ public class Rating implements Entity {
 	}
 
 	public String toCSV() {
-		return id + ";" + professionality + ";" + personality + ";" + mark;
+		return id + ";" + professionality + ";" + personality + ";" + general + ";" + note;
 	}
 
 	public String getKey() {
