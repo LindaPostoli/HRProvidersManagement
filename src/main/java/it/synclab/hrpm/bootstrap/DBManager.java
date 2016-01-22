dd5a34eef799661d5d4938abc4488d270cf07149
LindaPostoli <linda.postoli@gmail.com>
2016-01-22 16:36:38 +0100

    implemented method update(Entity obj)

--- src/main/java/it/synclab/hrpm/bootstrap/DBManager.java
+++ src/main/java/it/synclab/hrpm/bootstrap/DBManager.java
@@ -19,14 +19,13 @@ public class DBManager {
 
 	private static ConnectionPool cp;
 
-	public DBManager() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException{
+	public DBManager() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
 		cp = ConnectionPool.getInstance();
 	}
-	
-	
+
 	public static final void init()
 			throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
-		
+
 	}
 
 	public void insert(Entity obj) throws SQLException, NoSuchMethodException, SecurityException,
@@ -35,8 +34,6 @@ public class DBManager {
 		Connection conn = (Connection) cp.getConnection();
 
 		String query = "INSERT INTO " + obj.getClass().getSimpleName() + " VALUES(";
-
-		Method[] methods = obj.getClass().getMethods();
 
 		List<Field> fields = new ArrayList<Field>();
 		for (Field field : obj.getClass().getDeclaredFields()) {
@@ -69,35 +66,79 @@ public class DBManager {
 			System.out.println(methodName);
 
 			Method method = pst.getClass().getDeclaredMethod(methodName, paramTypes);
-		
+
 			fields.get(i).setAccessible(true);
-			method.invoke(pst, i+1, fields.get(i).get(obj));
+			method.invoke(pst, i + 1, fields.get(i).get(obj));
+
+		}
+
+		pst.executeUpdate();
+
+	}
+
+	public void update(Entity obj) throws InstantiationException, IllegalAccessException, ClassNotFoundException,
+			SQLException, NoSuchFieldException, SecurityException, NoSuchMethodException, IllegalArgumentException, InvocationTargetException {
+
+		Connection conn = (Connection) cp.getConnection();
+		String query = "UPDATE " + obj.getClass().getSimpleName() + " SET ";
+
+		List<Field> fields = new ArrayList<Field>();
+		for (Field field : obj.getClass().getDeclaredFields()) {
+			if (!field.getName().equalsIgnoreCase("HEADER"))
+				fields.add(field);
+		}
+
+		int k = 0;
+		while (k < fields.size() - 1) {
+			query += fields.get(k).getName() + " = ?,";
+			k++;
+		}
+		query += fields.get(fields.size() - 1).getName() + " = ? ";
+
+		query += " WHERE " + obj.getKeyName() + " = " + obj.getKey() + ";";
+		
+		PreparedStatement pst = conn.prepareStatement(query);
+		
+		for (int i = 0; i < fields.size(); i++) {
+
+			String methodName = null;
+			if (fields.get(i).getType().getSimpleName().equals("int"))
+				methodName = "set" + "Int";
+			else if (fields.get(i).getType().getSimpleName().equals("Calendar"))
+				// TODO: ricordarsi di convertire la data da Calendar a Date
+				// quando lo si passa al preparedStatement
+				methodName = "set" + "Date";
+			else
+				methodName = "set" + fields.get(i).getType().getSimpleName();
+
+			Class<?>[] paramTypes = { int.class, fields.get(i).getType() };
+			System.out.println(methodName);
+
+			Method method = pst.getClass().getDeclaredMethod(methodName, paramTypes);
+
+			fields.get(i).setAccessible(true);
+			method.invoke(pst, i + 1, fields.get(i).get(obj));
 
 		}
 		
 		pst.executeUpdate();
-
+		
 		
 	}
 	
-		
-	public void update(Entity obj) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
 	
-		
-	}
+	
 
 	public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException,
-			InvocationTargetException, SQLException, NoSuchMethodException, SecurityException, ParseException, InstantiationException, ClassNotFoundException {
-		
-		DBManager dbm=new DBManager();
-		Rating rating = new Rating(1,2,3,5);
-		JobWebsite jwb = new JobWebsite("www.indeed.com");
-		jwb.setName("Indeed");
+			InvocationTargetException, SQLException, NoSuchMethodException, SecurityException, ParseException,
+			InstantiationException, ClassNotFoundException, NoSuchFieldException {
+
+		DBManager dbm = new DBManager();
+		Rating rating = new Rating(31, 2, 3, 5);
 		dbm.insert(rating);
-		System.out.println("fatto");
-		dbm.insert(jwb);
-		System.out.println("fatto");
-		
+		rating.setGeneral(10);
+		dbm.update(rating);
+
 	}
 
 }
