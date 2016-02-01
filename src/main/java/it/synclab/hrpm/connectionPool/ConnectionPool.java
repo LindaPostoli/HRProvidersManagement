16a9ad79c1c81b402aa5835f2ecd90759bcd05b5
Giuseppe Cozzolino <giuseppe.patred@gmail.com>
2016-02-01 11:17:19 +0100

    start with hibernate

--- src/main/java/it/synclab/hrpm/connectionPool/ConnectionPool.java
+++ src/main/java/it/synclab/hrpm/connectionPool/ConnectionPool.java
@@ -1,62 +1,60 @@
 package it.synclab.hrpm.connectionPool;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Session;
+
+import it.synclab.hrpm.core.HibernateSessionService;
 import it.synclab.hrpm.exception.ConnectionNotFoundException;
 import it.synclab.hrpm.exception.FullConnectionPoolException;
 
 public class ConnectionPool {
-
+	
 	private static ConnectionPool connectionPool = null;
-	private List<Connection> listConnections;
+	private List<Session> listConnections;
 	private static final int NUM_CONNECTIONS = 10;
-
-	private ConnectionPool()
-			throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
-		listConnections = new ArrayList<Connection>();
+	
+	private ConnectionPool() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		listConnections = new ArrayList<Session>();
 		init();
 	}
-
+	
 	public void init() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
-		java.sql.Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/HRPM", "root",
-				"synclab");// FIXME: check username e password
-
+		HibernateSessionService hss = new HibernateSessionService();
+		Session session = hss.openSession(false);
 		for (int i = 0; i < NUM_CONNECTIONS; i++) {
-			listConnections.add(connection);
+			listConnections.add(session);
 		}
-
+		
 	}
-
-	public static ConnectionPool getInstance()
-			throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+	
+	public static ConnectionPool getInstance() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
 		if (connectionPool == null) {
 			connectionPool = new ConnectionPool();
 		}
 		return connectionPool;
 	}
-
-	public Connection getConnection() {
+	
+	public Session getConnection() {
 		if (listConnections.isEmpty())
 			throw new ConnectionNotFoundException("No more connections available");
-
+			
 		return listConnections.remove(0);
-
+		
 	}
-
-	public void releaseConnection(Connection connection) throws FullConnectionPoolException {
+	
+	public void releaseConnection(Session connection) throws FullConnectionPoolException {
 		if (listConnections.size() >= NUM_CONNECTIONS)
 			throw new FullConnectionPoolException("Connection Pool is already full");
-
+			
 		listConnections.add(connection);
 	}
-
+	
 	public int getConnectionNumberAvailable() {
 		return listConnections.size();
 	}
-
+	
 }
--- src/main/java/it/synclab/hrpm/services/RatingService.java
+++ src/main/java/it/synclab/hrpm/services/RatingService.java
@@ -2,13 +2,12 @@ package it.synclab.hrpm.services;
 
 import java.sql.SQLException;
 
-import it.synclab.hrpm.enumeration.ConnectionCriteria;
 import it.synclab.hrpm.model.Rating;
 
 public class RatingService extends PersistentService<Rating> {
-	
-	public RatingService(ConnectionCriteria criteria) throws ClassNotFoundException, SQLException {
-		super(criteria);
+
+	public RatingService() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		super();
 	}
-	
+
 }
