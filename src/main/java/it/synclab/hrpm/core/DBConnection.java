826938f8b882b72dc1785b38cae25891e9ef69bc
Giuseppe Cozzolino <giuseppe.patred@gmail.com>
2016-02-01 11:26:29 +0100

    fix to compile

--- src/main/java/it/synclab/hrpm/core/DBConnection.java
+++ src/main/java/it/synclab/hrpm/core/DBConnection.java
@@ -3,7 +3,6 @@ package it.synclab.hrpm.core;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 
-import it.synclab.hrpm.bootstrap.DBManager;
 import it.synclab.hrpm.exception.FullConnectionPoolException;
 import it.synclab.hrpm.model.Entity;
 
@@ -11,16 +10,12 @@ public class DBConnection implements Connection {
 
 		
 	public void insert(Entity obj) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, SQLException, InstantiationException, ClassNotFoundException, FullConnectionPoolException  {
-		DBManager dbm = new DBManager();
-		dbm.insert(obj);
 	}
 	
 	public void update(Entity obj) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
-		DBManager dbm = new DBManager();
 	}
 
 	public void delete(Entity obj) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
-		DBManager dbm = new DBManager();
 	}
 
 	
